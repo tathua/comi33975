3f60c4eb44d0167ec2f91bbf5b3068bac6a40402
Natalia Sal <88118132+tathua@users.noreply.github.com>
2022-07-09 15:52:35 -0300

    Clase1.js
    Solamente es el archivo JS.
    
    Variables (let y const)
    Mostar mje en consola y pantalla
    Guardar resultados

---
+++ clase1.js
@@ -0,0 +1,31 @@
+//clase 1
+//Variables 
+
+// LET cambia su valor
+//CONST no cambia su valor
+
+let edad = 12 // número entero
+let anio = "1956" // string
+const nombre = "Naty" //string
+const direccion = "Av. siempre viva 123" //string
+
+
+
+//Operaciones
+
+// + , - , / , * 
+//Sumar sin guardar
+let num1 = 100
+let num2 = 21
+alert(num1 + num2) //mje en pág
+console.log(num1 + num2) //mje en consola
+console.log("Bienvenid@s a nuestra app web")
+
+//Guardar resultado en una variable
+let suma = num1 + num2
+console.log("La suma de los números es: " +suma)
+
+
+//Generar un prompt y guardar su valor
+let nombreNuevo =  prompt("Ingrese su nombre") //pide datos al usuario
+console.log("tu nombre es: " + nombreNuevo)
